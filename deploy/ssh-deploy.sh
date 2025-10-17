db805ff08495b343d166ba0c23206defb9ac3b9a
zeinab tizkar <123273835+zizitizi@users.noreply.github.com>
2025-10-17 22:20:00 +0330

    Update ssh-deploy.sh

--- deploy/ssh-deploy.sh
+++ deploy/ssh-deploy.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+set -e
+DEPLOY_DIR=/home/odoo-deploy/deploy
+ssh -i ~/.ssh/id_deploy -o StrictHostKeyChecking=no odoo-deploy@${VPS_HOST} << 'EOF'
+cd $DEPLOY_DIR
+docker-compose pull
+docker-compose up -d --remove-orphans
+docker image prune -f
+EOF
